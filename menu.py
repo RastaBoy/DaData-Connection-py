84ef19c8fc0f0a82b78f63b5287d569feaa6fd31
Alexander Troyan <trustyone3224@gmail.com>
2021-06-13 18:05:35 +0600

    Improved menu system

--- menu.py
+++ menu.py
@@ -1,45 +1,20 @@
 import os
 from typing import Iterable
-from utils import get_addresses, get_coordinates
+from utils import get_addresses, get_coordinates, dict_formater, printer
 from db import con, get_settings, update_settings, default_settings
 from request import get_response
 
-main_menu = (
-    ("--- DaData Connection Py --- \n", ),
-    ("Сделать запрос", lambda: request_menu()),
-    ("Настройки", lambda: menu(settings)),
-    ("Выход", lambda: 0)
-)
 
-settings = (
-    ("--- Меню пользовательских настроек ---\n", ),
-    ("Показать текущие настройки", lambda: show_settings()),
-    ("Изменить URL", lambda: change_settings("URL")),
-    ("Изменить API", lambda: change_settings("API")),
-    ("Изменить язык", lambda: menu(language)),
-    ("Вернуть настройки по-умолчанию", lambda: default_settings(con)),
-    ("Назад", lambda: 0)
-)
 
-language = (
-    ("--- Меню выбора языка ---\n", ),
-    ("Русский", lambda: menu(dialogue_form(change_settings, ("language", "ru")))),
-    ("Английский", lambda: menu(dialogue_form(change_settings, ("language", "en")))),
-    ("Назад", lambda: 0) 
-)
 
-dialogue = (
-    ("Вы уверены?", ),
-    ("Да", lambda x: x()),
-    ("Нет", lambda: 0)
-)
-
-def dialogue_form(func, params:tuple):
-    return [
-        ("Вы уверены?", ),
-        ("Да", lambda x, params: x(*params), func, params),
-        ("Нет", lambda: 0)
-    ]
+def dialogue_form(text):
+    return {
+        'description' : text,
+        'menu_items' : [
+            ("Да", lambda: True),
+            ("Нет", lambda: False)
+        ] 
+    }
 
 def coordinates_menu(address:str):
     settings = get_settings(con)
@@ -57,28 +32,26 @@ def request_menu():
 
     addresses = get_addresses(response)
     if len(addresses) > 0:
-        address_menu = list()
-        address_menu.append(("Выберите подходящий вам адрес: ", ))
+        address_menu = dict()
+        address_menu['description'] = "Список адресов, подходящих под Ваш запрос:"
+        address_menu['menu_items'] = []
 
         for item in addresses:
-            address_menu.append((item, lambda x: print(f"Выбранный адрес находится на следующих координатах: {','.join(coordinates_menu(x))}"), item))
+            #address_menu['menu_items'].append((item, lambda x: print(f"Выбранный адрес находится на следующих координатах: {','.join(coordinates_menu(x))}"), item))
+            address_menu['menu_items'].append((item, lambda x: printer(f"Выбранный Вами адрес - {x}\nНаходится на следующих координатах: {','.join(coordinates_menu(x))}", result=0), item))
 
-        address_menu.append(("Назад", lambda: 0))
-        func = menu(address_menu)
-        os.system("pause")
-        return func
+        address_menu['menu_items'].append(("Назад", lambda: 0))
+        menu(address_menu)
     else: 
-        print("К сожалению, таких адресов не найдено...")
-        return 1
+        printer("К сожалению, таких адресов не найдено...")
 
 
 
 def show_settings():
     os.system('cls')
     settings = get_settings(con)
-    for key in settings:
-        print(f"{key} : {settings[key]}")  
-    return 1
+    printer(dict_formater(settings))
+
 
 
 def change_settings(key:str, value=None):
@@ -94,29 +67,72 @@ def change_settings(key:str, value=None):
         print("Что-то пошло не так...")
 
 
-def menu(menu:Iterable):
+def set_language(lang:str):
+    result = menu(dialogue_form("Вы уверены, что хотите изменить язык?"))
+    if result == True:
+        change_settings('language', lang)
+        printer('Язык был изменен!')
+    return 0
+
+
+main_menu = {
+    'description' : "--- DaData Connection Py ---",
+    'menu_items' : [
+        ("Сделать запрос", lambda: request_menu()),
+        ("Настройки", lambda: menu(settings)),
+        ("Выход", lambda: 0)
+    ]
+}
+
+settings = {
+    'description' : "--- Меню пользовательских настроек ---",
+    'menu_items' : [
+        ("Показать текущие настройки", show_settings),
+        ("Изменить URL", lambda: change_settings("URL") if menu(dialogue_form('Вы уверены, что хотите изменить URL?')) else None),
+        ("Изменить API", lambda: change_settings("API") if menu(dialogue_form('Вы уверены, что хотите изменить API?')) else None),
+        ("Изменить язык", lambda: menu(language)),
+        ("Вернуть настройки по-умолчанию", lambda: default_settings(con) if menu(dialogue_form('Вы уверены, что хотите вернуть настройки по-умолчанию?')) else None),
+        ("Назад", lambda: 0)
+    ]
+}
+
+language = {
+    'description' : "--- Меню выбора языка ---",
+    'menu_items' : [
+        ("Русский", set_language, 'ru'),
+        ("Английский", set_language, 'en'),
+        ("Назад", lambda: 0) 
+    ]
+}
+
+
+
+
+def menu(menu:dict):
+
+    # if res == None -> continue
+    # if res == 0 -> break
+    # else -> res 
+
     while True:
         os.system('cls')
         
-        for i, item in enumerate(menu):
-            if i == 0:
-                print(item[0])
-            else:
-                print(f"{i}. {item[0]}")
+        print(menu['description'])
+        for i, item in enumerate(menu['menu_items']):
+            print(f"{i+1}. {item[0]}")
 
         try:
             answer = int(input("Выберите пункт меню: "))
             
             if answer > 0:
-                item = menu[answer]
-                func = item[1](*item[2:])
-                if func is None:
-                    return -1
-                elif func == 0:
-                    return -1
-                elif func == 1:
-                    os.system("pause")
-                    pass
+                item = menu['menu_items'][answer-1]
+                result = item[1](*item[2:])
+                if result is None:
+                    continue
+                elif result == 0:
+                    break
+                else:
+                    return result
             else:
                 raise Exception("Некорректный ввод! Попробуйте еще раз...")
         except Exception as e: 
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os
+
 def get_addresses(response:dict):
     addresses = list()
     for item in response:
@@ -9,4 +11,18 @@ def get_addresses(response:dict):
 def get_coordinates(response:dict):
     response = response[0]['data']
     result = (response['geo_lat'], response['geo_lon'], )
+    return result
+
+
+def printer(text:str, *, result=None):
+    os.system('cls')
+    print(text)
+    os.system("pause")
+    return result
+
+
+def dict_formater(obj:dict):
+    result = ""
+    for key in obj:
+        result += f"'{key}' : '{obj[key]}' \n"
     return result
